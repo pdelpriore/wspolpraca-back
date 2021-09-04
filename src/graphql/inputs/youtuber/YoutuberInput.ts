03eeca7c643169431bb7c727272b53fd7c3f445c
pdelpriore <paul.del.priore29@gmail.com>
2021-09-04 10:55:20 +0200

    add youtuber input type

--- src/graphql/inputs/youtuber/YoutuberInput.ts
+++ src/graphql/inputs/youtuber/YoutuberInput.ts
@@ -1,5 +1,8 @@
-import { InputType, Field } from "type-graphql";
-import { Length } from "class-validator";
+import { InputType, Field, Int, Float } from "type-graphql";
+import { Length, MinLength } from "class-validator";
+import { CommentsYoutuberType } from "../../entities/comments/CommentsYoutuberType";
+import { BrandType } from "../../entities/user/BrandType";
+import { MessageType } from "../../entities/message/MessageType";
 
 @InputType()
 export class YoutuberInputType {
@@ -7,7 +10,7 @@ export class YoutuberInputType {
   userType!: string;
 
   @Field()
-  @Length(6, 255)
+  @MinLength(3)
   name!: string;
 
   @Field()
@@ -15,4 +18,52 @@ export class YoutuberInputType {
 
   @Field()
   password!: string;
+
+  @Field({ nullable: true })
+  photo?: string;
+
+  @Field({ nullable: true })
+  isEmailConfirmed?: boolean;
+
+  @Field({ nullable: true })
+  category?: string;
+
+  @Field({ nullable: true })
+  youtubeUrl?: string;
+
+  @Field({ nullable: true })
+  channelBornDate?: Date;
+
+  @Field(() => Int, { nullable: true })
+  subscribersQuantity?: number;
+
+  @Field(() => Float, { nullable: true })
+  meanViewsNumberPerVideo?: number;
+
+  @Field(() => Float, { nullable: true })
+  meanSubscribersAge?: number;
+
+  @Field(() => Int, { nullable: true })
+  subscriberMaleSexQuantity?: number;
+
+  @Field(() => Int, { nullable: true })
+  subscriberFemaleSexQuantity?: number;
+
+  @Field(() => Float, { nullable: true })
+  meanLikesNumberPerVideo?: number;
+
+  @Field(() => Float, { nullable: true })
+  meanCommentsNumberPerVideo?: number;
+
+  @Field({ nullable: true })
+  creationDate?: Date;
+
+  @Field(() => [CommentsYoutuberType], { nullable: true })
+  comments?: CommentsYoutuberType[];
+
+  @Field(() => [BrandType], { nullable: true })
+  companiesInCooperation?: BrandType[];
+
+  @Field(() => [MessageType], { nullable: true })
+  messages?: MessageType[];
 }
