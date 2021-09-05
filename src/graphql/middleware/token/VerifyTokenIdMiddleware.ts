aa550b2d941c616b0ba2bc66525a23f8b3068063
pdelpriore <paul.del.priore29@gmail.com>
2021-09-05 20:22:21 +0200

    clean code

--- src/graphql/middleware/token/VerifyTokenIdMiddleware.ts
+++ src/graphql/middleware/token/VerifyTokenIdMiddleware.ts
@@ -4,7 +4,7 @@ import { IContext } from "../../context/Context";
 export const verifyTokenIdMiddleware: MiddlewareFn<IContext> = (
   { context: { req } },
   next
-): any => {
+) => {
   const tokenId = req.get("x-auth");
   console.log(tokenId);
   return next();
