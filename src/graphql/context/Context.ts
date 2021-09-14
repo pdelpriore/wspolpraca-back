5e09f191f178a442b02fc3d74d6a505eedd0aabf
pdelpriore <paul.del.priore29@gmail.com>
2021-09-14 20:09:29 +0200

    clean code

--- src/graphql/context/Context.ts
+++ src/graphql/context/Context.ts
@@ -1,26 +1,5 @@
 import { Request, Response } from "express";
 
-type TUserData = {
-  userType: string;
-  name: string;
-  email: string;
-};
-
-export interface IUserDataVariables {
-  isGoogleUser: boolean;
-  signupUserData: TUserData;
-}
-
-interface ISignup extends Request {
-  body: {
-    variables: IUserDataVariables;
-  };
-}
-
-export interface ISignupContext {
-  req: ISignup;
-}
-
 export interface IContext {
   req: Request;
   res: Response;
