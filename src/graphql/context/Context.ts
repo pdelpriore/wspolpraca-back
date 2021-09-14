86d1910173c1d4bb7d04b4b22c778a4adb10c838
pdelpriore <paul.del.priore29@gmail.com>
2021-09-14 19:59:56 +0200

    add new ctx

--- src/graphql/context/Context.ts
+++ src/graphql/context/Context.ts
@@ -1,5 +1,26 @@
 import { Request, Response } from "express";
 
+type TUserData = {
+  userType: string;
+  name: string;
+  email: string;
+};
+
+export interface IUserDataVariables {
+  isGoogleUser: boolean;
+  signupUserData: TUserData;
+}
+
+interface ISignup extends Request {
+  body: {
+    variables: IUserDataVariables;
+  };
+}
+
+export interface ISignupContext {
+  req: ISignup;
+}
+
 export interface IContext {
   req: Request;
   res: Response;
